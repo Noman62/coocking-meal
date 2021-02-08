d98aa329f71277152c0878fbb0cfb43fbc783cfb
Asadullah Al Noman <noman.ice.nstu@gmail.com>
2021-02-08 18:34:32 +0600

    creating cooking meal

--- apps.js
+++ apps.js
@@ -2,37 +2,39 @@ document.getElementById('searchBtn').addEventListener("click", function () {
 
     const mealList = document.getElementById('mealList');
     let getInputText = document.getElementById('getInput').value;
-    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${getInputText}`)
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${getInputText}`)
         .then(res => res.json())
         .then(data => {
 
-            data.meals.forEach(meal => {
+            if (data.meals) {
+                data.meals.forEach(meal => {
 
-                const div = document.createElement('div');
-                div.className = 'meal-item';
-                const mealInfo = `
-                <div onclick="displayDetails('${meal.idMeal}')"<div>
-                    <img src="${meal.strMealThumb}" alt="food">
-                    <h3>${meal.strMeal}<h3>
-                </div>
-                `;
+                    const div = document.createElement('div');
+                    div.className = 'meal-item';
+                    const mealInfo = `
+                            <div onclick="displayDetails('${meal.idMeal}')"<div>
+                                <img src="${meal.strMealThumb}" alt="food">
+                                <h3>${meal.strMeal}<h3>
+                            </div>
+                            `;
 
-                div.innerHTML = mealInfo;
-                mealList.appendChild(div);
+                    div.innerHTML = mealInfo;
+                    mealList.appendChild(div);
+                });
+            }
+            else {
+                document.getElementById('errorMessage').innerHTML = "Sorry, we didn't find any meal!";
 
-
-            });
+            }
 
 
         })
-        .catch(err => notFound(err))
+
+    document.getElementById('IngredientOfMeal').innerHTML = "";
+    document.getElementById('mealList').innerHTML = "";
+    document.getElementById('errorMessage').innerHTML = "";
     document.getElementById('getInput').value = "";
 })
-
-//print error message
-const notFound = err => {
-    alert("Sorry, we didn't find any meal!");
-}
 
 //get recipe of the meal
 const displayDetails = id => {
@@ -43,24 +45,16 @@ const displayDetails = id => {
 
 // create a modal
 const detailsPerMeal = item => {
-    const instructionDiv = document.getElementById('IngredientOfMeal');
-    instructionDiv.innerHTML = `
+    const mealDiv = document.getElementById('IngredientOfMeal');
+    mealDiv.innerHTML = `
     <img src="${item.strMealThumb}" alt="food">
     <h3>${item.strMeal}<h3>
     <h3>Ingredients</h3>
-     
     <li>${item.strIngredient1}</li>
-         
     <li>${item.strIngredient2}</li>
-         
     <li>${item.strIngredient3}</li>
-         
     <li>${item.strIngredient4}</li>
-         
     <li>${item.strIngredient5}</li>
-         
     <li>${item.strIngredient6}</li>
-         
-    <li>${item.strIngredient7}</li>
-     `;
+   `;
 }
